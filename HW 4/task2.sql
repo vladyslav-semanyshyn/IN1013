8baee308092ba9ed3a3dfed43689de2b15eabdeb
vladyslav-semanyshyn <Vladyslav.Semanyshyn@city.ac.uk>
2024-10-27 19:49:43 +0000

    Add files via upload

---
+++ HW 4/task2.sql
@@ -0,0 +1,18 @@
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) VALUES 
+('Fluffy', '2020-10-15', 'vet', 'antibiotics');
+
+INSERT INTO petPet (petname, owner, species, gender, birth, death) VALUES
+('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL);
+
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) VALUES
+('Hammy', '2020-10-15', 'vet', 'antibiotics');
+
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) VALUES
+('Fluffy', '2020-10-15', 'litter', '5 kittens, 2 male, 3 female');
+
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) VALUES
+('Claws', '1997-08-03', 'vet', 'broken rib');
+
+UPDATE petPet SET death = '2020-09-01' WHERE petname = 'Puffball';
+
+DELETE FROM petPet WHERE owner = 'Harold' AND species = 'dog';
